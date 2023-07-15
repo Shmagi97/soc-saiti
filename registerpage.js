1b042b9d0fa440bf83d756b93764371b9450814e
Shmagi97 <135329382+Shmagi97@users.noreply.github.com>
2023-07-15 17:26:43 +0400

    javascript registration add
    davamate registraciis funqciis nawili ar aris dasrulebuli

--- registerpage.js
+++ registerpage.js
@@ -17,3 +17,36 @@ const registrationFocus = document.getElementById("registrationJs");
 registrationFocus.addEventListener("click", () => {
   document.getElementById("InputSaxeli").focus();
 });
+
+const sheyvaniliTexti = [];
+
+const registrationSubmitButton = document.getElementById("registrationSubmit1");
+
+// registrationSubmitButton.addEventListener("click", () => {
+//   const inputValue = document.getElementById("InputSaxeli");
+//   const inputSaxeliSheyvanili = inputValue.value;
+//   sheyvaniliTexti.push(inputSaxeliSheyvanili);
+//   console.log(sheyvaniliTexti);
+// });
+
+// const sheyvaniliNumber = [];
+
+registrationSubmitButton.addEventListener("click", () => {
+  const inputNumber = document.getElementById("InputGvari");
+  const inputValue = document.getElementById("InputSaxeli");
+
+  const inputNumberValue = parseInt(inputNumber.value);
+  const inputSaxeliSheyvanili = inputValue.value;
+
+  // if (!isNaN(inputNumberValue)) {
+  //   sheyvaniliTexti.push(inputNumberValue);
+  //   console.log(sheyvaniliTexti);
+  // } else {
+  //   sheyvaniliTexti.push(inputSaxeliSheyvanili);
+  //   console.log(sheyvaniliTexti);
+  // }
+
+  sheyvaniliTexti.push(inputNumberValue);
+  sheyvaniliTexti.push(inputSaxeliSheyvanili);
+  console.log(sheyvaniliTexti);
+});
